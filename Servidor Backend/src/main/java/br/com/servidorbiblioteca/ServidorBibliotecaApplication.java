0358bb059a7923021ef25ab9c11d35ac80a4d59a
pedrorodriguesct <chronotechnologyinc@gmail.com>
2020-10-21 17:15:33 -0300

    ADICIONA+SPRINGBOOT+BACKEND

---
+++ Servidor Backend/src/main/java/br/com/servidorbiblioteca/ServidorBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.servidorbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorBibliotecaApplication.class, args);
+	}
+
+}
---
+++ Servidor Backend/src/test/java/br/com/servidorbiblioteca/ServidorBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.servidorbiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
