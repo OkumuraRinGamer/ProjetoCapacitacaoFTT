e26b4965f826300c25715499658c7d8ce360586b
pedrorodriguesct <chronotechnologyinc@gmail.com>
2020-10-22 21:21:43 -0300

    ADICIONA+ANGULAR+MATERIAL+SISTEMA+DE+ROTAS

--- Biblioteca Frontend/src/app/app-routing.module.ts
+++ Biblioteca Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentLayoutComponent } from './layouts/content-layout.component';
+import { CONTENT_ROUTE } from './shared/routes/content-routes.routes';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: CONTENT_ROUTE,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
